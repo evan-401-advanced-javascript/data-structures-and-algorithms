9add538792d566c66da0046ec0a1b9bedd0c48e3
Evan Brecht-Curry <starfoxz13@gmail.com>
2019-08-15 22:02:57 -0700

    inital commit

---
+++ challenges/cc1/array-reverse.js
@@ -0,0 +1,16 @@
+'use strict'
+
+const arrayReverse = () => {
+let array = [1,2,3,4,-5];
+let middle = array.length / 2;
+let arrStart;
+
+for (let i = 0; i < middle; i++) {
+    arrStart = array[i]
+    array[i] = array[array.length - i - 1];
+    array[array.length - i - 1] = arrStart;
+}
+console.log(array)
+};
+
+arrayReverse();
