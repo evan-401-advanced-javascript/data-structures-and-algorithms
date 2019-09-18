6e5712df30693720943a546fdad1ba216d78e7be
Evan Brecht-Curry <starfoxz13@gmail.com>
2019-09-18 07:11:09 -0700

    adding testing

--- challenges/cc27-find-largest/__tests__/cc27.test.js
+++ challenges/cc27-find-largest/__tests__/cc27.test.js
@@ -1,36 +1,37 @@
 'use strict';
 
 const largestNode = require('../index');
+const Node = require('../Node');
 
 
 describe('validator module performs basic validation of', () => {
 
-  const Node = require('../Node');
 
-  const ten = new Node(10);
-  const seven = new Node(7);
-  const five = new Node(5);
-  const one = new Node(1);
-  const two = new Node(2);
-  const eleven = new Node(11);
-  const twelve = new Node(12);
-  const twenty = new Node(20);
-
-  five.left = one;
-  five.right = two;
-
-  ten.left = seven;
-  ten.right = five;
-
-  seven.left = eleven;
-  seven.right = twelve;
-
-  one.right = twenty;
-
-
-  let largest = null;
 
   it('validates if largest node is found', () => {
+
+    const ten = new Node(10);
+    const seven = new Node(7);
+    const five = new Node(5);
+    const one = new Node(1);
+    const two = new Node(2);
+    const eleven = new Node(11);
+    const twelve = new Node(12);
+    const twenty = new Node(20);
+
+    five.left = one;
+    five.right = two;
+
+    ten.left = seven;
+    ten.right = five;
+
+    seven.left = eleven;
+    seven.right = twelve;
+
+    one.right = twenty;
+
+    let largest = null;
+
     expect(largestNode(ten).toEqual(20))
   });
 });
